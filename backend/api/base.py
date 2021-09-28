61e20e46763991f71fd400a5c41632b44aba24e9
Paul Smith <paulrsmithjnr@gmail.com>
2021-09-28 17:02:34 -0500

    Created API endpoint for creating a job post

--- backend/api/base.py
+++ backend/api/base.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from api.version1 import route_users
+from api.version1 import route_jobs
 
 api_router = APIRouter()
 
 api_router.include_router(route_users.router, prefix="/users", tags=["users"])
+api_router.include_router(route_jobs.router, prefix="/job", tags=["jobs"])
