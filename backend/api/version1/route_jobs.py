9c7306393f8e1b5a4cda7ca4fda59c27af7d40c1
Paul Smith <paulrsmithjnr@gmail.com>
2021-09-28 18:25:39 -0500

    Created endpoint to fetch all available jobs

--- backend/api/version1/route_jobs.py
+++ backend/api/version1/route_jobs.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from typing import List
 
 from db.session import get_db
 from db.models.jobs import Job
 from schemas.jobs import JobCreate, ShowJob
-from db.repository.jobs import create_new_job, retrieve_job
+from db.repository.jobs import create_new_job, retrieve_job, list_jobs
 
 router = APIRouter()
 
@@ -14,9 +15,14 @@ def create_job(job: JobCreate, db: Session = Depends(get_db)):
     job = create_new_job(job=job, db=db, owner_id=owner_id)
     return job
 
-@router.get("/get/{id}" , response_model=ShowJob)
+@router.get("/get/{id}", response_model=ShowJob)
 def retrieve_job_by_id(id: int, db: Session = Depends(get_db)):
     job = retrieve_job(id=id, db=db)
     if not job:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id {id} does not exist")
     return job
+
+@router.get("/all", response_model=List[ShowJob])
+def retrieve_all_jobs(db: Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return jobs
