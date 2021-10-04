22ebaa61afc2b904f6a1baf9c5d150d81b3fa553
Paul Smith <paulrsmithjnr@gmail.com>
2021-10-04 15:41:15 -0500

    Created endpoint for logging in

--- backend/api/base.py
+++ backend/api/base.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from api.version1 import route_users
 from api.version1 import route_jobs
+from api.version1 import route_login
 
 api_router = APIRouter()
 
 api_router.include_router(route_users.router, prefix="/users", tags=["users"])
 api_router.include_router(route_jobs.router, prefix="/job", tags=["jobs"])
+api_router.include_router(route_login.router, prefix="/login", tags=["login"])
