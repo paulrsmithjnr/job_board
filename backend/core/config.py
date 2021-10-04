749ba4d4a914fa51fecb3853adbddee7dc46f790
Paul Smith <paulrsmithjnr@gmail.com>
2021-10-04 15:07:47 -0500

    JWT generation

--- backend/core/config.py
+++ backend/core/config.py
@@ -16,4 +16,7 @@ class Settings:
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "db_course")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
+    SECRET_KEY: str =os.getenv("SECRET_KEY")
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
 settings = Settings()
